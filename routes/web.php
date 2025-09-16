06695cec488e4f3da7c607c37c5c6336932dec24
Gaelen <gaelenpro@gmail.com>
2025-09-16 11:21:35 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoucherController;
 
-Route::get('/voucher', [VoucherController::class, 'index'])->name('voucher.index');
+Route::get('/', [VoucherController::class, 'index'])->name('voucher.index');
 Route::post('/voucher/search', [VoucherController::class, 'search'])->name('voucher.search');
 Route::post('/voucher/process', [VoucherController::class, 'process'])->name('voucher.process');
 Route::get('/voucher/winners', [VoucherController::class, 'winners'])->name('voucher.winners');
